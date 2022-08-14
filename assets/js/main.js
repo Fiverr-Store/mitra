6134b67bcd0a179a74acda45676be3c99efb8d5c
ali-dev-code <ali.asgr514@gmail.com>
2022-08-14 21:50:09 +0500

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,25 @@
 (function () {
 
-  var toggle = document.getElementById("theme-toggle");
+  // var toggle = document.getElementById("theme-toggle");
 
-  var storedTheme =
-    localStorage.getItem("theme") ||
-    (window.matchMedia("(prefers-color-scheme: dark)").matches
-      ? "dark"
-      : "light");
-  if (storedTheme)
-    document.documentElement.setAttribute("data-theme", storedTheme);
+  // var storedTheme =
+  //   localStorage.getItem("theme") ||
+  //   (window.matchMedia("(prefers-color-scheme: dark)").matches
+  //     ? "dark"
+  //     : "light");
+  // if (storedTheme)
+  //   document.documentElement.setAttribute("data-theme", storedTheme);
 
-  toggle.onclick = function () {
-    var currentTheme = document.documentElement.getAttribute("data-theme");
-    var targetTheme = "light";
+  // toggle.onclick = function () {
+  //   var currentTheme = document.documentElement.getAttribute("data-theme");
+  //   var targetTheme = "light";
 
-    if (currentTheme === "light") {
-      targetTheme = "dark";
-    }
-    document.documentElement.setAttribute("data-theme", targetTheme);
-    localStorage.setItem("theme", targetTheme);
-  };
+  //   if (currentTheme === "light") {
+  //     targetTheme = "dark";
+  //   }
+  //   document.documentElement.setAttribute("data-theme", targetTheme);
+  //   localStorage.setItem("theme", targetTheme);
+  // };
 
 
  //===== text-blink
